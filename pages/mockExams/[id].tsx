728f8637879f04885a113cb4696e1c4f7941c513
eungwang <scm323@naver.com>
2022-10-23 02:20:21 +0900

    Feat: MockExams detail page generate

--- pages/mockExams/[id].tsx
+++ pages/mockExams/[id].tsx
@@ -1,10 +1,31 @@
-import { useRouter } from 'next/router';
+import { Button } from 'antd';
 import React from 'react';
 import Layout from '../../src/components/layout/Layout';
+import MockExamQuestion from '../../src/components/mockExamQuestion/MockExamQuestion';
+import { useReadMockExam } from '../../src/lib/hooks/useMockExams';
 
 const MockExamsDetail = () => {
-  const router = useRouter();
-  return <Layout>{router.query.id}</Layout>;
+  const { data, loading } = useReadMockExam();
+
+  if (!data || loading) return null;
+  const { readMockExam } = data;
+  return (
+    <Layout>
+      <div className="flex gap-8">
+        <h2 className="text-2xl my-auto ">{readMockExam.mockExam.title}</h2>
+        <Button
+          size="large"
+          disabled={readMockExam.mockExam.approved}
+          className="w-28"
+        >
+          {readMockExam.mockExam.approved ? '게시중' : '승인하기'}
+        </Button>
+      </div>
+      {readMockExam.mockExam.mockExamQuestion.map((question) => (
+        <MockExamQuestion key={question.id} question={question} />
+      ))}
+    </Layout>
+  );
 };
 
 export default MockExamsDetail;
