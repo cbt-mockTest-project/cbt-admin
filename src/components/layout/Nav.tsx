3d60f907d1f02b6d2efc28d851c290ca50dc14eb
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-10-26 15:07:40 +0900

    Feat: navbar 로그인상태 로직 변경

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,11 +1,22 @@
+import { deleteCookie } from 'cookies-next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useMe } from '../../lib/hooks/useAuth';
+import { authRoutes } from '../../lib/routes';
+import { authTokenVar } from '../../modules/apollo';
+import ClientOnly from '../common/ClientOnly';
 
 const Nav = () => {
   const router = useRouter();
+  const { refetch } = useMe();
   const isRoot = router.pathname === '/';
   const isManage = router.pathname === '/manage';
+  const onLogout = () => {
+    authTokenVar('');
+    deleteCookie('jwt-token');
+    refetch();
+  };
   return (
     <nav className="w-full border-b border-solid border-slate-200 flex justify-center py-6">
       <div className="flex justify-between max-w-screen-xl w-full">
@@ -29,14 +40,29 @@ const Nav = () => {
             </span>
           </Link>
         </div>
-        <div className="flex gap-5">
-          <span className="cursor-pointer hover:text-cyan-500 transition-colors">
-            로그인
-          </span>
-          <span className="cursor-pointer hover:text-cyan-500 transition-colors">
-            회원가입
-          </span>
-        </div>
+        <ClientOnly>
+          {authTokenVar() ? (
+            <span
+              onClick={onLogout}
+              className="cursor-pointer hover:text-cyan-500 transition-colors"
+            >
+              로그아웃
+            </span>
+          ) : (
+            <div className="flex gap-5">
+              <Link href={authRoutes.login}>
+                <span className="cursor-pointer hover:text-cyan-500 transition-colors">
+                  로그인
+                </span>
+              </Link>
+              <Link href={authRoutes.register}>
+                <span className="cursor-pointer hover:text-cyan-500 transition-colors">
+                  회원가입
+                </span>
+              </Link>
+            </div>
+          )}
+        </ClientOnly>
       </div>
     </nav>
   );
