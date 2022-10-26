50ed90122b1696c92e07636588f61438714b486d
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-10-26 15:06:09 +0900

    Feat: fragment & mutation & query 모음 파일 제작

---
+++ src/lib/grapql/mutation.ts
@@ -0,0 +1,31 @@
+import { gql } from "@apollo/client";
+
+export const CreateMockExam_Mutation = gql`
+  mutation CreateMockExam($input: CreateMockExamInput!) {
+    createMockExam(input: $input) {
+      error
+      ok
+    }
+  }
+`;
+
+export const Delet_Mock_Exam_Question_Feedback_Mutation = gql`
+  mutation DeleteMockExamQuestionFeedback(
+    $input: DeleteMockExamQuestionFeedbackInput!
+  ) {
+    deleteMockExamQuestionFeedback(input: $input) {
+      error
+      ok
+    }
+  }
+`;
+
+export const LOGIN_MUTATION = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      ok
+      error
+      token
+    }
+  }
+`;
---
+++ src/lib/grapql/query.ts
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+import { FULL_QUESTION_FRAGMENT } from './fragment';
+
+export const ReadAllMockExamCategory_Query = gql`
+  query ReadAllMockExamCategories {
+    readAllMockExamCategories {
+      categories {
+        name
+      }
+    }
+  }
+`;
+
+export const ReadAllMockExam_QUERY = gql`
+  query ReadAllMockExam($input: ReadAllMockExamsInput!) {
+    readAllMockExam(input: $input) {
+      mockExams {
+        title
+        id
+      }
+    }
+  }
+`;
+
+export const SearchMockExam_QUERY = gql`
+  query SearchMockExam($input: SearchMockExamInput!) {
+    searchMockExam(input: $input) {
+      mockExams {
+        id
+        title
+      }
+    }
+  }
+`;
+
+export const ReadMockExam_Query = gql`
+  query ReadMockExam($input: ReadMockExamInput!) {
+    readMockExam(input: $input) {
+      mockExam {
+        title
+        approved
+        mockExamQuestion {
+          ...FullQuestionParts
+        }
+      }
+    }
+  }
+  ${FULL_QUESTION_FRAGMENT}
+`;
+
+export const ME_QUERY = gql`
+  query Me {
+    me {
+      id
+      nickname
+    }
+  }
+`;
