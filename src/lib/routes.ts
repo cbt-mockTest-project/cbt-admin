4e0e754fb3116ccd68fb1d7129cc85efac2c5f4c
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-10-26 15:00:47 +0900

    Feat: add authRoutes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -2,3 +2,8 @@ export const MockExamRoutes = {
   write: '/mockExams/write',
   detail: (id: number) => `/mockExams/${id}`,
 };
+
+export const authRoutes = {
+  login: '/auth/login',
+  register: '/auth/register',
+};
