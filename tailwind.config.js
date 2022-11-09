76d326f1629b215b0b337b8785f89e7745c5cb2b
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-11-09 10:47:03 +0900

    Doc: tailwind config 수정 & axios package 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
